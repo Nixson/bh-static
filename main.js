d07e9e55efc1b2e91904658c7ff18f74c909401d
nixson <admin@nixson.ru>
2015-12-17 15:05:51 +0300

    auto

--- main.js
+++ main.js
@@ -39,8 +39,9 @@ bHelp = (function(){
 			cBhBlock.id = 'cMil_Line';
 			cBhBlock.setAttribute('src','data:image/png;base64,'+_this.get.lineImg);
 			window.parent.document.body.appendChild(cBhBlock);
-			$( '#cMil_Line', window.parent.document ).hide();
-			$( '#cMil_Line', window.parent.document ).show("drop",1000);
+			$( '#cMil_Line', window.parent.document ).hide(function(){
+				$( '#cMil_Line', window.parent.document ).show("drop",1000);
+			});
 			$('#cMil_Line', window.parent.document).on('click',function(){
 				_this.fadeIn();
 			});
