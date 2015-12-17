146231f17db0642515ff2b4c36f592b3394be590
nixson <admin@nixson.ru>
2015-12-17 12:14:33 +0300

    auto

--- mini.js
+++ mini.js
@@ -12,7 +12,7 @@
 	idocument.close();
 	var jsel = document.createElement('SCRIPT');
 	jsel.type = 'text/javascript';
-	jsel.innerHTML = 'bhelpSrvAddress="'+bhelpSrvAddress+'/main.js",bhelpSrvVersion='+bhelpSrvVersion+';if(window["localStorage"] === null){ var bhelpVersion = localStorage.getItem("bhelp_version"), bl="bhelp_latest"; if(bhelpVersion==null || bhelpVersion != bhelpSrvVersion) bhelpLoad(bhelpSrvAddress,bl); else eval(localStorage.getItem(bl));} else bhelpLoad(bhelpSrvAddress,bl); function bhelpLoad(url,name){ if (window.XMLHttpRequest) xmlhttp=new XMLHttpRequest(); else xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); xmlhttp.onreadystatechange=function(){if (xmlhttp.readyState==4 && xmlhttp.status==200){if(window["localStorage"] === null) {localStorage.setItem(name,xmlhttp.responseText);} eval(xmlhttp.responseText);}}; xmlhttp.open("GET", url, false ); xmlhttp.send();}';
+	jsel.innerHTML = 'bhelpSrvAddress="'+bhelpSrvAddress+'",bhelpSrvId='+bhelpSrvId+',bhelpInfoAddress="'+bhelpInfoAddress+'",ba=bhelpSrvAddress+"/main.js",bhelpSrvVersion='+bhelpSrvVersion+';if(window["localStorage"] === null){ var bhelpVersion = localStorage.getItem("bhelp_version"), bl="bhelp_latest"; if(bhelpVersion==null || bhelpVersion != bhelpSrvVersion) bhelpLoad(ba,bl); else eval(localStorage.getItem(bl));} else bhelpLoad(ba,bl); function bhelpLoad(url,name){ if (window.XMLHttpRequest) xmlhttp=new XMLHttpRequest(); else xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); xmlhttp.onreadystatechange=function(){if (xmlhttp.readyState==4 && xmlhttp.status==200){if(window["localStorage"] === null) {localStorage.setItem(name,xmlhttp.responseText);} eval(xmlhttp.responseText);}}; xmlhttp.open("GET", url, false ); xmlhttp.send();}';
 	idocument.body.appendChild(jsel);
 	return {}
 }());
