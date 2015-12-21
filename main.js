1df5b854a081ea917505b7fd036aa2b77660745d
nixson <admin@nixson.ru>
2015-12-21 13:41:15 +0300

    auto

--- main.js
+++ main.js
@@ -88,9 +88,8 @@ bHelp = (function(){
 			$.each(_this.managerList,function(i,v){
 				manList[i] = {img:v.img.version_img,block:v.block.version_block};
 			});
-			console.log(manList);
-			$.post(bhelpInfoAddress+"/"+_this.sid+"/"+_this.client+"/"+window.parent.document.location.hostname,{agent:navigator.userAgent,url:window.parent.document.location.pathname,title:window.parent.document.title,os:navigator.platform,ref:window.parent.document.referrer,mid:_this.mid,time:_this.firstTime,managers:JSON.stringify(manList)},function(resp){
-				var rsp = JSON.parse(resp);
+			$.post(bhelpInfoAddress+"/"+_this.sid+"/"+_this.client+"/"+window.parent.document.location.hostname,{agent:navigator.userAgent,url:window.parent.document.location.pathname,title:window.parent.document.title,os:navigator.platform,ref:window.parent.document.referrer,mid:_this.mid,time:_this.firstTime,managers:JSON.stringify(manList)},function(rsp){
+//				var rsp = JSON.parse(resp);
 				console.log(rsp);
 				_this.client = rsp.uid;
 				_this.online = rsp.onlien;
@@ -108,7 +107,7 @@ bHelp = (function(){
 				_this.Storage.setItem('bhelp_managerList',JSON.stringify(_this.managerList));
 
 				console.log(rsp);
-			});
+			},'json');
 //			$.post(bhelpInfoAddress+"/"+parent[bhelpSrvId]+_this.client+)
 
 //			_this.load(bhelpInfoAddress+"/")
