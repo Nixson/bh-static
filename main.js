5f6c30286451da5697acf4261c5536641e78f296
nixson <admin@nixson.ru>
2015-12-22 10:50:25 +0300

    auto

--- main.js
+++ main.js
@@ -323,7 +323,7 @@ bHelp = (function(){
 				cBhBlock.setAttribute('src','data:image/png;base64,'+_this.get.lineImg);
 			} else{
 				var lineImg;
-				$.each(_this.managerList,function(i,v){lineImg=v.block.content; break;});
+				$.each(_this.managerList,function(i,v){lineImg=v.block.content;});
 				cBhBlock.setAttribute('src','data:image/png;base64,'+lineImg);
 			}
 			window.parent.document.body.appendChild(cBhBlock);$( '#cMil_Line', window.parent.document ).show("drop",300);$('#cMil_Line', window.parent.document).on('click',function(){_this.fadeIn();});var div = document.createElement('div');div.id = 'cMil_body';div.style.visibility='hidden'; div.style.display='none'; div.innerHTML = '<div id="cMil_FrameCover" style="position: fixed; top: 0; left: 0; display: block;"><div id="cMil_FrameClose"></div><div id="cMil_FrameSound"></div></div>'; window.parent.document.body.appendChild(div); $('#cMil_body',window.parent.document).width($(window.parent).width() - 20).height(window.parent.innerHeight - 282).show();$(window.parent).resize(function () {$('#cMil_body',window.parent.document).width($(window.parent).width() - 20).height(window.parent.innerHeight - 282);});
