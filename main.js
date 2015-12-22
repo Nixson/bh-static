27d14f97d9298f8a6c1c1bb7720fcb439778f958
nixson <admin@nixson.ru>
2015-12-22 10:47:08 +0300

    auto

--- main.js
+++ main.js
@@ -319,12 +319,11 @@ bHelp = (function(){
 		initLine: function(){//инициализация кнопки вызова и блока для drag
 			var _this = this; _this.insertLine(_this.get.lineStyle+mainStyle);
 			var cBhBlock = document.createElement('img');cBhBlock.id = 'cMil_Line';
-			console.log($.isEmptyObject(_this.managerList),_this.managerList);
-			/*if(_this.online && !$.isEmptyObject(_this.managerList)){
+			if(_this.online && !($.isEmptyObject(_this.managerList))){
 				var lineImg;
 				$.each(_this.managerList,function(i,v){lineImg=v.block.content; break;});
 				cBhBlock.setAttribute('src','data:image/png;base64,'+lineImg);
-			} else*/
+			} else
 				cBhBlock.setAttribute('src','data:image/png;base64,'+_this.get.lineImg);
 			window.parent.document.body.appendChild(cBhBlock);$( '#cMil_Line', window.parent.document ).show("drop",300);$('#cMil_Line', window.parent.document).on('click',function(){_this.fadeIn();});var div = document.createElement('div');div.id = 'cMil_body';div.style.visibility='hidden'; div.style.display='none'; div.innerHTML = '<div id="cMil_FrameCover" style="position: fixed; top: 0; left: 0; display: block;"><div id="cMil_FrameClose"></div><div id="cMil_FrameSound"></div></div>'; window.parent.document.body.appendChild(div); $('#cMil_body',window.parent.document).width($(window.parent).width() - 20).height(window.parent.innerHeight - 282).show();$(window.parent).resize(function () {$('#cMil_body',window.parent.document).width($(window.parent).width() - 20).height(window.parent.innerHeight - 282);});
 			$('#cMil_FrameCover',window.parent.document).draggable({containment: '#cMil_body',
