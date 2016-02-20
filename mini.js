c4fdf6c09c7724d83de6ca841cfeb17e0bc1c8de
nixson <admin@nixson.ru>
2016-02-20 10:17:51 +0300

    auto

--- mini.js
+++ mini.js
@@ -18,7 +18,7 @@
 	jsel.type = 'text/javascript';
 	var html = "";
 	if(!mv || mv>=8){html +='var bhelpVersion = localStorage.getItem("bhelp_version"); if(bhelpVersion==null || bhelpVersion != bhelpVer) bhelpLoad(ba,bl); else eval(localStorage.getItem(bl));';}else{html +="bhelpLoad(ba,bl);";}if(mv && mv <9){html +='function bhelpLoad(url,name){url=url.replace("https://","http://"); var xmlhttp=new XDomainRequest(); xmlhttp.onload=function(){if (xmlhttp.readyState==4 && xmlhttp.status==200){if(window["localStorage"] != null) {localStorage.setItem(name,xmlhttp.responseText);localStorage.setItem("bhelp_version",bhelpVer);} eval(xmlhttp.responseText);}}; xmlhttp.open("GET", url); xmlhttp.send();}';}else {html +='function bhelpLoad(url,name){var xmlhttp=new XMLHttpRequest(); xmlhttp.onreadystatechange=function(){if (xmlhttp.readyState==4 && xmlhttp.status==200){localStorage.setItem(name,xmlhttp.responseText);localStorage.setItem("bhelp_version",bhelpVer); eval(xmlhttp.responseText);}}; xmlhttp.open("GET", url, true ); xmlhttp.send();}';}
-	jsel.innerHTML = 'bhelpSrvAddress="'+bhelpSrvAddress+'",bhelpSrvId='+bhelpSrvId+',bhelpInfoAddress="'+bhelpInfoAddress+'",ba=bhelpSrvAddress+"/main.js",bhelpSrvVersion='+bhelpSrvVersion+',bhelpVer='+bhelpVer+',bl="bhelp_latest";'+html;
+	jsel.innerHTML = 'bhelpSrvAddress="'+bhelpSrvAddress+'",bhelpSrvId='+bhelpSrvId+',bhelpInfoAddress="'+bhelpInfoAddress+'",ba=bhelpSrvAddress+"/main.js?'+bhelpVer+'",bhelpSrvVersion='+bhelpSrvVersion+',bhelpVer='+bhelpVer+',bl="bhelp_latest";'+html;
 	idocument.body.appendChild(jsel);
 	return {};
 }());
