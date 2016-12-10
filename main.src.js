cd2e01fe7cf0cf5aa96c7189b7e7368132073b73
Nixson <admin@nixson.ru>
2016-12-10 14:38:00 +0300

    auto

--- main.src.js
+++ main.src.js
@@ -181,7 +181,7 @@ var animate = function (doc, win) {
 			self.history[uid].left = 0;
 		if(self.history[uid].top < 0)
 			self.history[uid].top = 0;
-		console.log("show",self.history[uid].top,windowSize.height);
+		console.log("show",self.history[uid].top,id.clientHeight,windowSize.height);
 		self.addRule(uid, function(){
 			id.style.top = self.history[uid].top+"px";
 			id.style.left = self.history[uid].left+"px";
