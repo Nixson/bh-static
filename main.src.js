867f5dcf19743274b7e3cdbf7763ab53ffb051e5
Nixson <admin@nixson.ru>
2016-12-10 14:45:17 +0300

    auto

--- main.src.js
+++ main.src.js
@@ -171,8 +171,8 @@ var animate = function (doc, win) {
 		self.history[uid].top = parseInt(self.history[uid].top);
 		self.history[uid].left = parseInt(self.history[uid].left);
 		var windowSize = {width: self.cWidth(), height: self.cHeight()};
-		console.log("show",ignore,self.history[uid].top,id.clientHeight,windowSize.height);
 		if(!ignore){
+			console.log("show",ignore,self.history[uid].top,id.clientHeight,windowSize.height);
 			if(windowSize.width < self.history[uid].left+id.clientWidth)
 				self.history[uid].left = windowSize.width - id.clientWidth - 10;
 			if(windowSize.height < self.history[uid].top+id.clientHeight)
